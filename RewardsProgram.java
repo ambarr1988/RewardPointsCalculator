10abb455fe5188b0e83fea500ce5d4ea9b884cda
ambarr1988 <ambarr1988@gmail.com>
2019-05-22 12:23:52 -0500

    adjusted the package
    Now there is no package

--- RewardsProgram.java
+++ RewardsProgram.java
@@ -1,23 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *author Aaron Barr
+ * for an interview project
+ * 
  */
-package rewardsprogram;
-
-
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-/**
- *author Aaron Barr
- * for an interview project
- * 
- */
+
 public class RewardsProgram {
 
     public static void main(String[] args) {
